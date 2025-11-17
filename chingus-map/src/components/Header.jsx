940386b4ee4c81aa493aedc6191ebf3e46ca216b
NadiiaLashtun <flor.smile11@gmail.com>
2025-11-17 21:04:39 +0100

    Create basic navigation with react router

--- chingus-map/src/components/Header.jsx
+++ chingus-map/src/components/Header.jsx
@@ -1,5 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
 function Header() {
-  return <h2>Header</h2>;
+  return (
+    <header>
+      <nav>
+        <ul className='flex justify-center gap-6 p-6'>
+          <li>
+            <NavLink to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='map'>Map</NavLink>
+          </li>
+          <li>
+            <NavLink to='list'>List</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
