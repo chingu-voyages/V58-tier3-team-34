cacf0abbce0fc7d8d2766bf235eacc53c4e8b59b
NadiiaLashtun <flor.smile11@gmail.com>
2025-11-17 17:54:55 +0100

    Add basic components

---
+++ chingus-map/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+      &#169; 2025
+      <a href='https://github.com/chingu-voyages/V58-tier3-team-34'>
+        #GitHub Chingu\'s Map
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ chingus-map/src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <h1>Sorry, the page you were looking for was not found</h1>;
+}
+
+export default NotFound;
