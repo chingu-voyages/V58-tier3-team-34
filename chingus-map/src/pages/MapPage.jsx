3f6050a559fdcfce3a4a301ecacb4a09603103ce
Nadiia Lashtun <flor.smile11@gmail.com>
2025-11-21 16:00:39 +0100

    Add json with chingus info. Create function to calculate members in each country

--- chingus-map/src/pages/MapPage.jsx
+++ chingus-map/src/pages/MapPage.jsx
@@ -1,5 +1,34 @@
+import MemberList from '../data/chingu_info.json';
+
+function countCountryMembers(memberList) {
+  return memberList.reduce((countriesArray, member) => {
+    const country = member['Country name (from Country)'];
+
+    //if country field is empty - just return list
+    if (!country) return countriesArray;
+
+    //if country not exist in list (undefined or null) - create with 0
+    countriesArray[country] ??= 0;
+    countriesArray[country] += 1;
+
+    return countriesArray;
+  }, {});
+}
+
 function MapPage() {
-  return <h1>MapPage</h1>;
+  const countriesList = countCountryMembers(MemberList);
+  return (
+    <div>
+      <h1>MapPage</h1>
+      <ul className='overflow-auto max-h-screen'>
+        {Object.keys(countriesList).map((key) => (
+          <li key={key}>
+            {key}: {countriesList[key]}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default MapPage;
